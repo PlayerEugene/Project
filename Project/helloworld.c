22ce01a12c9c106c54bbdcc26ee130ba50e37aa8
Eugene Lee <eugenehlee99@gmail.com>
2020-06-06 02:19:48 -0700

    Finished Hello World

--- Project/helloworld.c
+++ Project/helloworld.c
@@ -3,9 +3,17 @@
 
 void main() {
 	char k[40];
-	printf("What would you like to say?");
-	fgets(k, 20, stdin);
-	if (strcmp(k, "Hello World!"))
-		printf("Hello User!");
+	while (strcmp(k, "exit\n")) {
+		printf("What would you like to say? ");
+		fgets(k, 20, stdin);
+		if (!strcmp(k, "Hello World!\n"))
+			printf("Hello User!\n");
+		else if (!strcmp(k, "Hello Me!\n"))
+			printf("Hello You!\n");
+		else if (!strcmp(k, "exit\n")) {
 
+		}
+		else
+			printf("Try another phrase!");
+}
 }
