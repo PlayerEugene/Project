e90dc63a98bcf4dc7832d5d7dcc871a03ca6bdda
Eugene Lee <eugenehlee99@gmail.com>
2020-06-10 20:31:28 -0700

    Updates Hello World to include enum/fixed layout

--- Project/helloworld.c
+++ Project/helloworld.c
@@ -1,19 +1,68 @@
 #include <stdio.h>
 #include <string.h>
+#define LENGTH 40
+
+/*basic Hello World C file*/
+
+typedef enum
+{
+	HELLO_USER,
+	HELLO_YOU,
+	ERROR,
+	EXIT,
+} output_e;
 
 void main() {
-	char k[40];
-	while (strcmp(k, "exit\n")) {
-		printf("What would you like to say? ");
-		fgets(k, 20, stdin);
-		if (!strcmp(k, "Hello World!\n"))
-			printf("Hello User!\n");
-		else if (!strcmp(k, "Hello Me!\n"))
-			printf("Hello You!\n");
-		else if (!strcmp(k, "exit\n")) {
+	char input[LENGTH];
+	int flag = 0;
+	int state = 0;
+	output_e output;
 
+	while (!state) {
+		output = ERROR;
+		printf("What would you like to say? ");
+		fgets(input, LENGTH, stdin);
+		if (!strcmp(input, "Hello World!\n"))
+		{
+			if (flag == 0)
+			{
+				output = HELLO_USER;
+			}
+			else
+			{
+				output = HELLO_YOU;
+			}
 		}
-		else
-			printf("Try another phrase!");
-}
+		else if (!strcmp(input, "Hello Me!\n"))
+		{
+			output = HELLO_YOU;
+			flag = 1;
+		}
+		else if (!strcmp(input, "exit\n")) {
+			output = EXIT;
+			state = 1;
+		}
+		
+		switch (output)
+		{
+		case HELLO_USER:
+			printf("Hello User!\n");
+			break;
+
+		case HELLO_YOU:
+			printf("Hello You!\n");
+			break;
+
+		case ERROR:
+			printf("Try another phrase!\n");
+			break;
+
+		case EXIT:
+			break;
+
+		default:
+			printf("Try another phrase!\n");
+			break;
+		}
+	}
 }
